bb521d539a48923fb7fb48f0446400a7641e7554
IsaacWP121 <47909717+IsaacWP121@users.noreply.github.com>
2021-04-09 16:09:35 +1000

    cool

---
+++ randomstatus.py
@@ -0,0 +1,4 @@
+from random import choice
+def randomstatus():
+    l = ["Jason Getting Banned", "Jacob Collier do his magic again", "Our community grow", "f Chat", "Some, videos....", "YOU SLEEP", "My grades fall", "The polar ice melt"]
+    return choice(l)
